5e9cf282c460a3ed54848c8a090bd3d1a01f0c49
clobochar <clo.bochar_@hotmail.com>
2022-10-12 12:40:11 +0100

    Add "Fizz" condition

---
+++ index.js
@@ -0,0 +1,15 @@
+const ul = document.querySelector("ul.card-list");
+const card = document.querySelector("li.card");
+
+for (let index = 1; index <= 100; index++) {
+  let card_class = "card";
+  let card_text = index;
+
+  if (index % 3 == 0) {
+    card_class += " fizz";
+    card_text = "Fizz";
+  }
+
+  const card = `<li class="${card_class}">${card_text}</li>`;
+  ul.innerHTML += card;
+}
