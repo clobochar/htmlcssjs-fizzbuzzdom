89dcca325de8289e49e233f8880c7f978c3966c9
clobochar <clo.bochar_@hotmail.com>
2022-10-12 12:42:30 +0100

    Add "Buzz" condition

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ for (let index = 1; index <= 100; index++) {
     card_text = "Fizz";
   }
 
+  if (index % 5 == 0) {
+    card_class += " buzz";
+    card_text = "Buzz";
+  }
+
   const card = `<li class="${card_class}">${card_text}</li>`;
   ul.innerHTML += card;
 }
